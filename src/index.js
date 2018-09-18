445a82b7b05f88fc355fe3d57f04143985b242ab
ForiDunk <Szabolcs.forreiter@gmail.com>
2018-09-18 23:59:48 +0300

    add api key and youtube-api-search

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+const API_KEY = 'AIzaSyCQLSa1un6HOlaJKMqVbX4vukNs5zGmD38';
+
 const App = () => {
   return <div>Hi!</div>;
 };
